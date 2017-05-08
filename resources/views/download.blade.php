aa578d011953f822f069326b9e2c7deaed311582
rexixuel <rexixuel@users.noreply.github.com>
2017-05-08 14:32:20 +0800

    Added modals for deletion warning

--- resources/views/download.blade.php
+++ resources/views/download.blade.php
@@ -1,5 +1,25 @@
 @extends('layouts.app',['title' => 'Download'])
 @section ('content')
+		<!-- Small modal -->		
+		<div class="modal fade alert alert-warning deleteWarning" tabindex="-1" role="dialog" aria-labelledby="delete">
+		  <div class="modal-dialog modal-sm" role="document">
+		    <div class="modal-content">
+		      <div class="modal-header">
+			<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+			<h4 class="modal-title" id="modalLabel">Deleting File...</h4>
+		      </div>
+		      <div class="modal-body">
+		         <p id="deleteWarning"> </p> 
+		      </div>
+		      <div class="modal-footer">
+		        <form class="form form-inline" method="POST" action="delete" role="form">
+				<button type="button" class="btn btn-link" data-dismiss="modal">Cancel</button>		
+				<button type="submit" class="btn btn-danger" name="deleteId">Delete</button>
+			</form>
+		      </div>		      
+		    </div>
+		  </div>
+		</div>
 		<div class="container">	
 
 			<div class="pageTitle">
@@ -27,6 +47,11 @@
 					</div>					
 				</form>				
 			</div>
+			@if (session('message'))
+				<div class="alert alert-success">
+					{!! session('message') !!}
+				</div>
+			@endif
 			<div class="moduleList">
 				<table class="table table-striped table-hover table-responsive">
 					<thead>
@@ -46,7 +71,12 @@
 						<td> {{ $module->created_at }} </td>
 						<td> <a href="download/{{$module->id}}" class="btn btn-primary"> <i class="fa fa-cloud-download" aria-hidden="true"></i> </a> </td>
 						@if(Auth::user()->role < 1)
-							<td> <a href="delete/{{$module->id}}" class="btn btn-danger"> <i class="fa fa-trash" aria-hidden="true"></i> </a> </td>
+							<td> <a href="delete/{{$module->id}}" 
+							        class="btn btn-danger delete" data-toggle="modal" data-target=".deleteWarning" data-title="{{ $module->title }}"
+								data-id="{{$module->id}}">
+							     	<i class="fa fa-trash" aria-hidden="true"></i> 
+							     </a> 
+							</td>
 						@endif
 					  </tr>
 					</tbody>
