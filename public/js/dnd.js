28f96fa04364e58132d6c1cb526c82382b031255
rexixuel <rexixuel@users.noreply.github.com>
2017-05-08 15:36:26 +0800

    Update dnd.js

--- public/js/dnd.js
+++ public/js/dnd.js
@@ -4,7 +4,19 @@ custom scripts for team dimaunahan project
 
 **/
 $(document).ready(function(){
-
+	
+	$('#deleteWarning').on('show.bs.modal', function (event) {
+	  var button = $(event.relatedTarget); // Button that triggered the modal
+	  var title = button.data('title'); // Extract title from data-* attributes
+	  var id = button.data('id'); // Extract id from data-* attributes
+	  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+	  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+	  var modal = $(this);
+	  modal.find('#deleteWarningMessage').text(title + ' shall be deleted from server. Are you sure you want to continue?' );
+	  modal.find('#deleteId').val(id);
+	})
+	
+	
 	if (!($('.boxFile').files))
 	{
 		$('.fileMeta').hide();
