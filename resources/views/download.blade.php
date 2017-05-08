a3c995359b24fe29e25a59ece060a8237b3f376f
rexixuel <rexixuel@users.noreply.github.com>
2017-05-08 15:42:35 +0800

    Update download.blade.php

--- resources/views/download.blade.php
+++ resources/views/download.blade.php
@@ -13,6 +13,7 @@
 		      </div> <!-- modal body -->
 		      <div class="modal-footer">
 		        <form class="form form-inline" method="POST" action="delete" role="form">
+				{{ csrf_field() }}
 				<button type="button" class="btn btn-link" data-dismiss="modal">Cancel</button>		
 				<button type="submit" class="btn btn-danger" name="deleteId" id="deleteId">Delete</button>
 			</form>
