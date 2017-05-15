27d10cbefcef13842edd45d26caa488b1af24cd5
rexixuel <rexixuel@users.noreply.github.com>
2017-05-15 11:20:41 +0800

    Add 2017-XXXXX dummy recs

--- database/seeds/TestScript.php
+++ database/seeds/TestScript.php
@@ -60,6 +60,17 @@ class TestScript extends Seeder
 		$valid_student->create(['student_number' => '2015-90227']);
 		$valid_student->create(['student_number' => '2015-90305']);
 		$valid_student->create(['student_number' => '2015-90628']);
+	        // 2017 Dummy
+		$valid_student->create(['student_number' => '2017-00001']);
+		$valid_student->create(['student_number' => '2017-00002']);
+		$valid_student->create(['student_number' => '2017-00003']);
+		$valid_student->create(['student_number' => '2017-00004']);
+		$valid_student->create(['student_number' => '2017-00005']);
+		$valid_student->create(['student_number' => '2017-00006']);
+		$valid_student->create(['student_number' => '2017-00007']);
+		$valid_student->create(['student_number' => '2017-00008']);
+		$valid_student->create(['student_number' => '2017-00009']);
+		$valid_student->create(['student_number' => '2017-00010']);	    
 
 
 		//super users
