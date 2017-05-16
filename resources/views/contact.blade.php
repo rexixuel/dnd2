5baf80836ed977d23a8a475edd423c1695ff12b4
rexixuel <rexixuel@users.noreply.github.com>
2017-05-16 10:14:02 +0800

    fix contact break lines

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -6,8 +6,12 @@
 					<h2> CONTACT INFORMATION </h2>
 					<h3> Compass Team </h3>
 					<p class="text-justify flavorText">
-						Technology Management Center 
+						Technology Management Center
+					</p>
+					<p class="text-justify flavorText">
 						University of the Philippines 
+					</p>
+					<p class="text-justify flavorText">
 						ASTI Building, Carlos P. Garcia Avenue, Diliman, Quezon City 1101 Philippines 
 					</p>
 					<p class="text-justify flavorText">
